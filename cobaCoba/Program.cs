939dcf5aacad8f69a4c2cc40cdfd9494fc748e5d
vanessachristie <vchristie@student.ciputa.ac.id>
2021-12-03 10:47:56 +0700

    nambahin cara cek bilangan prima

--- cobaCoba/Program.cs
+++ cobaCoba/Program.cs
@@ -6,7 +6,25 @@ namespace cobaCoba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int statusProgram = 1;
+            while (statusProgram == 1)
+            {
+                int HasilBagi = 0;
+                Console.Write("Masukkan angka: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                int m = input / 2;
+                for (int i = 2; i <= m; i++)
+                {
+                    if (input % i == 0)
+                    {
+                        HasilBagi += 1;
+                    }
+                }
+                if (HasilBagi == 0)
+                    Console.WriteLine("Angka prima");
+                else
+                    Console.WriteLine("Bukan angka prima");
+            }   
         }
     }
 }
