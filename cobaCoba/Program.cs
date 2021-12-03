3d9e40fd69f24ca8eda63e8579209cad2d0f757c
Vanessa <Vanessa@DESKTOP-UDVTOJF>
2021-12-03 09:57:33 +0700

    Add project files.

---
+++ cobaCoba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cobaCoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
